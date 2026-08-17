052a5b1bf147d9e6d0b0e7a4805fb8926f7d80e1
residentialplotsandvillas-hub <residentialplotsandvillas@gmail.com>
2026-08-17 14:48:14 +0530

    first commit

---
+++ app/components/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Script from "next/script";
+
+const GA_ID = "G-0CTLW8HY53";
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
